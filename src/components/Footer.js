25ee27bf64f7a42f75fee63cf43533b280ab1f2f
Adriel <adriel.fariasan@gmail.com>
2022-03-26 15:53:15 -0300

    Adiciona css de Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,42 +1,46 @@
 import { Github, Linkedin } from 'react-bootstrap-icons';
+import Container from './Container';
+import '../styles/Footer.css';
 
 const Footer = () => {
   const year = new Date().getFullYear();
   return (
     <footer>
-      <div className='links'>
-        <p>
-          Adriel Faria, {year}
-          <span>
+      <Container>
+        <div className='links'>
+          <p>
+            Adriel Faria, {year}
+            <span>
+              <a
+                href='https://www.github.com/br-adriel'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <Github size={22} />
+              </a>
+              <a
+                href='https://www.linkedin.com/in/adriel-fsantos'
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <Linkedin size={22} />
+              </a>
+            </span>
+          </p>
+        </div>
+        <div className='resources'>
+          <p>
+            All the images used on this page were taken from{' '}
             <a
-              href='https://www.github.com/br-adriel'
+              href='https://www.themoviedb.org/'
               target='_blank'
               rel='noopener noreferrer'
             >
-              <Github size={22} />
+              The Movie Database
             </a>
-            <a
-              href='https://www.linkedin.com/in/adriel-fsantos'
-              target='_blank'
-              rel='noopener noreferrer'
-            >
-              <Linkedin size={22} />
-            </a>
-          </span>
-        </p>
-      </div>
-      <div className='resources'>
-        <p>
-          All the images used on this page were taken from{' '}
-          <a
-            href='https://www.themoviedb.org/'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            The Movie Database
-          </a>
-        </p>
-      </div>
+          </p>
+        </div>
+      </Container>
     </footer>
   );
 };
