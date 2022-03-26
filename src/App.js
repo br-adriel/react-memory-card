4e85c69112a9fd13154b56133aeaa7c77265c929
Adriel <adriel.fariasan@gmail.com>
2022-03-26 12:19:14 -0300

    Implementa exibicao de Cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import Header from './components/Header';
 import './App.css';
 import { useState } from 'react';
+import Main from './components/Main';
+import ea0 from './img/easy/0.jpg';
+import ea1 from './img/easy/1.jpg';
+import ea2 from './img/easy/2.jpg';
+import ea3 from './img/easy/3.jpg';
+import ea4 from './img/easy/4.jpg';
+import ea5 from './img/easy/5.jpg';
+import ea6 from './img/easy/6.jpg';
+import ea7 from './img/easy/7.jpg';
+import uniqid from 'uniqid';
 
 function App() {
   const [score, setScore] = useState(0);
@@ -13,7 +23,62 @@ function App() {
   // function for manipulating highscore
   const newHighscore = () => setHighscore(score);
 
-  return <Header score={score} highscore={highscore} />;
+  // niveis
+  const easy = [
+    {
+      img: ea0,
+      alt: 'Spider-Man: no way home',
+      id: uniqid(),
+    },
+    {
+      img: ea1,
+      alt: 'Spider-Man',
+      id: uniqid(),
+    },
+    {
+      img: ea2,
+      alt: 'Spider-Man: Homecoming',
+      id: uniqid(),
+    },
+    {
+      img: ea3,
+      alt: 'The Amazing Spider-Man',
+      id: uniqid(),
+    },
+    {
+      img: ea4,
+      alt: 'Spider-Man 3',
+      id: uniqid(),
+    },
+    {
+      img: ea5,
+      alt: 'The Amazing Spider-Man 2',
+      id: uniqid(),
+    },
+    {
+      img: ea6,
+      alt: 'Spider-Man: Far From Home',
+      id: uniqid(),
+    },
+    {
+      img: ea7,
+      alt: 'Spider-Man 2',
+      id: uniqid(),
+    },
+  ];
+
+  const levels = [
+    {
+      name: 'Easy',
+      images: [...easy],
+    },
+  ];
+  return (
+    <>
+      <Header score={score} highscore={highscore} />
+      <Main level={levels[0]} />
+    </>
+  );
 }
 
 export default App;
--- src/components/Main.js
+++ src/components/Main.js
@@ -2,12 +2,17 @@ import Card from './Card';
 import '../styles/Main.css';
 
 const Main = (props) => {
+  const images = props.level.images.sort(() => Math.random() - 0.5);
   return (
     <main>
       <div className='level'>
-        <h2>Level: Easy</h2>
+        <h2>Level: {props.level.name}</h2>
       </div>
-      <div className='game'></div>
+      <div className='game'>
+        {images.map((image) => {
+          return <Card img={image.img} alt={image.alt} key={image.id} />;
+        })}
+      </div>
     </main>
   );
 };
